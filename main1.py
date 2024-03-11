a9f09c6ed1d1734f3d7563f3f270766583610fa4
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2024-03-11 09:12:29 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,84 @@
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+import numpy as np
+import pytesseract
+from datetime import datetime
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+model = YOLO('best.pt')
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        point = [x, y]
+        print(point)
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap = cv2.VideoCapture('mycarplate.mp4')
+
+my_file = open("coco1.txt", "r")
+data = my_file.read()
+class_list = data.split("\n") 
+
+area = [(35, 375), (16, 456), (1015, 451), (965, 378)]
+
+count = 0
+list1 = []
+processed_numbers = set()
+
+# Open file for writing car plate data
+with open("car_plate_data.txt", "a") as file:
+    file.write("NumberPlate\tDate\tTime\n")  # Writing column headers
+
+while True:    
+    ret, frame = cap.read()
+    count += 1
+    if count % 2 != 0:
+        continue
+    if not ret:
+       break
+   
+    frame = cv2.resize(frame, (1020, 500))
+    results = model.predict(frame)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
+   
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        
+        d = int(row[5])
+        c = class_list[d]
+        cx = int(x1 + x2) // 2
+        cy = int(y1 + y2) // 2
+        result = cv2.pointPolygonTest(np.array(area, np.int32), ((cx, cy)), False)
+        if result >= 0:
+           crop = frame[y1:y2, x1:x2]
+           gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+           gray = cv2.bilateralFilter(gray, 15, 17, 17)
+
+           text = pytesseract.image_to_string(gray).strip()
+           text = text.replace('(', '').replace(')', '').replace(',', '')
+           if text not in processed_numbers:
+              processed_numbers.add(text) 
+              list1.append(text)
+              current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+              with open("car_plate_data.txt", "a") as file:
+                   file.write(f"{text}\t{current_datetime}\n")
+                   cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                   cv2.imshow('crop', crop)
+
+    print(list1)  
+    cv2.polylines(frame, [np.array(area, np.int32)], True, (255, 0, 0), 2)
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()    
+cv2.destroyAllWindows()
